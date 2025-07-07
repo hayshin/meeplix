f25a563fdc37166bc4a6429a88ba4b1488b2b9b5
hayshin <hayshinbj@gmail.com>
2025-07-07 20:38:01 +0500

    gemini try

--- ai/gemini.ts
+++ ai/gemini.ts
@@ -3,9 +3,13 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
+const models = {
+  gemini_flash_lite: "models/gemini-2.5-flash-lite-preview-06-17",
+};
 async function main() {
+  // const response = await ai.models.list();
   const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash-001",
+    model: models.gemini_flash_lite,
     contents: "Why is the sky blue?",
   });
   console.log(response.text);
